e46004e072fbfd4ef02e14a13be8209c4b148adb
indextrown <indextrown@gmail.com>
2026-03-27 00:03:51 +0900

    first commit

---
+++ AppStore/AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by 김동현 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MyPageView()
+        }
+    }
+}
---
+++ AppStore/AppStoreTests/AppStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreTests.swift
+//  AppStoreTests
+//
+//  Created by 김동현 on 3/26/26.
+//
+
+import Testing
+@testable import AppStore
+
+struct AppStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
